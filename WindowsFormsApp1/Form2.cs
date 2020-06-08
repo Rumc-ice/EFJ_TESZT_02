d112b0acec9058924a1e43cd0c31fb19082f7720
Rumi Tamas <rumi.tamas@epdb.hu>
2020-06-08 16:01:27 +0200

    form 2 tomika

--- WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/Form2.cs
@@ -14,5 +14,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Király Viktor"); 
+        }
     }
 }
