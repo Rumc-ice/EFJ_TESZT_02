e05e27574384ee23c5018a13154176376427cb8d
Rumi Tamas <rumi.tamas@epdb.hu>
2020-06-09 08:48:46 +0200

    space

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        public static string space(int db)
+        {
+            string ret = "";
+            for (int i = 0; i > db; i++) ret += " ";
+            return ret;
+        }
+    }
+}
